d76b4c0bc58d1f0c6c7af0611ac112dcb26aac9b
OSPK <waqas@opensource.com.pk>
2016-06-10 17:07:27 +0500

    dione

--- app/views.py
+++ app/views.py
@@ -2,9 +2,13 @@ from flask import request, render_template
 from .models import Universities, Programs
 from app import app
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cities = ['Abbottabad', 'Bagh', 'Bahawalpur', 'Bannu', 'Bhimber', 'Charsadda', 'D.I.Khan', 'Dera Ghazi Khan', 'Dir', 'Faisalabad', 'Gilgit', 'Gujranwala', 'Gujrat', 'Haripur', 'Hyderabad', 'Islamabad', 'Jamshoro', 'Karachi', 'Karak', 'Khairpur', 'Khuzdar', 'Kohat', 'Kotli', 'Lahore', 'Larkana', 'Lasbela', 'Loralai', 'Malakand', 'Manshera', 'Mardan', 'Mirpur', 'Multan', 'Muzaffarabad', 'Nawabshah', 'Nerain Sharif', 'Nowshera', 'Peshawar', 'Quetta', 'Rahim Yar Khan', 'Rawalakot', 'Rawalpindi', 'Sakrand', 'Sargodha', 'Sialkot', 'Sukkur', 'Swabi', 'Swat', 'Tandojam', 'Taxila', 'Topi', 'Turbat', 'Wah']
+    provinces = ["Islamabad", "Khyber Pakhtunkhwa", "Punjab", "Sindh", "Balochistan", "Azad Jammu and Kashmir", "Gilgit-Baltistan"]
+    return render_template('index.html', cities=cities, provinces=provinces)
+
 
 @app.route('/universities')
 def universities():
@@ -19,6 +23,7 @@ def universities():
         universities = Universities.query.filter_by(pub_pri=request.args.get('pub_pri')).all()
     return render_template('universities.html', cities=cities, provinces=provinces, universities=universities)
 
+
 @app.route('/programs/<int:uni_id>')
 def programs(uni_id):
     university = Universities.query.get(uni_id)
@@ -31,3 +36,15 @@ def program(id):
     program = Programs.query.filter_by(id=id).first()
     university = Universities.query.get(program.uni_id)
     return render_template('program.html', university=university, program=program)
+
+
+@app.route('/programs/scholarships')
+def scholarship():
+    programs = Programs.query.filter(Programs.scholarship is not False).all()
+    return render_template('scholarships.html', programs=programs, university=None)
+
+
+@app.route('/programs/')
+def all_programs():
+    programs = Programs.query.filter(Programs.degree is not False, Programs.title is not False).all()
+    return render_template('all_programs.html', programs=programs)
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gunicorn -b 0.0.0.0:5000 --worker-class=gevent -t 99999 --workers=10 --max-requests 1000 --timeout 28 app:app
