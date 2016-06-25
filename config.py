603562870b37b0edfadfdb54296878bbef4a4969
ospk <waqas@opensource.com.pk>
2016-06-25 18:13:40 +0500

    done

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = 'pymysql://root:x3nonx4@localhost/karwan'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:x3nonx4@localhost/karwan'
 
 DEBUG = True
 
